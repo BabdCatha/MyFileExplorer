29ecff4c1b78e20fe03c154d3aded48a2cdccb18
MicrowavedCat <38229200+MicrowavedCat@users.noreply.github.com>
2018-04-30 22:36:48 +0200

    Minor update
    Adding canvas for backward/forward/reload buttons

--- main.py
+++ main.py
@@ -70,15 +70,16 @@ can1=Canvas(width=x,height=y,highlightthickness=0,scrollregion=(0,0,x,y*1.5),bg=
 can1.pack()
 can1.bind("<Double-Button-1>", GetFileName)
 can2=Canvas(width=x/6-3,heigh=y/5*4,highlightthickness=0,bg="grey") #-3 : alignement avec le bloc notes (/!\ qui ne marche pas)
-can2.place(x=0,y=0)
+can2.place(x=0,y=y/20)
+can3=Canvas(width=x/6-3,heigh=y/20,highlightthickness=0,bg="gray8")
+can3.place(x=0,y=0)
+can3.create_line((x/6-3)/3,y/20,(x/6-3)/3,x/30,width=2,fill="gray99")
+can3.create_line((x/6-3)/3*2,y/20,(x/6-3)/3*2,x/30,width=2,fill="gray99")
 
 scrollbar1.config(command=can1.yview)
 
 can1['yscrollcommand'] = scrollbar1.set
 
-#places=can1.create_rectangle(0,0,x/6,y/5*4,fill="grey")
-#files=can1.create_rectangle(x/6,0,x-18,y,fill="black")
-#notes=can1.create_rectangle(0,y/5*4,x/6,y,fill="white")
 
 testEntry=Text(win1,height=10,width=29)
 testEntry.place(x=1,y=y/5*4+1)
